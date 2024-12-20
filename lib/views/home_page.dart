c89905df3c36eb42dc41f4a921143f1585392ab7
Divarizky <divarizky28@gmail.com>
2024-12-20 19:51:31 +0700

    membangun home page aplikasi dan menambahkan models note ke dalam proyek.
    home_page.dart:
    1. membuat heading pada home page terdiri dari "My Notes" dan Icon Button favorite.
    2. menambahkan search bar di halaman utama

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -3,14 +3,60 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // final noteViewModel = Provider.of<NoteViewModel>(context);
-
     return Scaffold(
-      appBar: AppBar(title: const Text('My Notes')),
+      backgroundColor: Colors.grey.shade900,
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [Container(child: const Text('Buat Catatan Baru'))],
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              // Heading Section
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'My Notes',
+                    style: TextStyle(fontSize: 24, color: Colors.white),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Container(
+                      width: 40,
+                      height: 40,
+                      decoration: BoxDecoration(
+                        color: Colors.grey.shade800,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Icon(Icons.favorite, color: Colors.white),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+
+              // Search Bar Section
+              TextField(
+                style: TextStyle(fontSize: 16, color: Colors.white),
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.symmetric(vertical: 12),
+                  hintText: 'Search notes',
+                  hintStyle: TextStyle(color: Colors.grey),
+                  prefixIcon: Icon(Icons.search, color: Colors.grey),
+                  fillColor: Colors.grey.shade800,
+                  filled: true,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(40),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(40),
+                    borderSide: const BorderSide(color: Colors.transparent),
+                  ),
+                ),
+              ),
+
+              // Floating Button
+            ],
+          ),
         ),
       ),
     );
